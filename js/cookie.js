58d0e76dabbb67faaf04dbf2486bf9a44010fcdc
radoslav <radoslaw.begej@gmail.com>
2017-10-14 17:34:15 +0200

    page

---
+++ js/cookie.js
@@ -0,0 +1,6 @@
+<script>
+	for (var i = 1; i <= 150; i++)
+		document.querySelector("#fillme").innerHTML += "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ";
+
+</script>
+
